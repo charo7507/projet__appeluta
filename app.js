a6d3f799586913a152cbb494b719094296cb1970
charo7507 <charolite.technologie@gmail.com>
2022-09-20 16:31:17 +0200

    finalisation de la page home

--- app.js
+++ app.js
@@ -1,6 +1,21 @@
 const oppenToggle = document.querySelector('.menu--item')
 const topww = document.querySelector('.top')
 
+
+
+const modale = document.querySelector('.modale')
+const openModale = document.querySelector('.home__btn')
+const closeModale = document.querySelector('.icon__close')
+
 oppenToggle.addEventListener('click', () =>{
     topww.classList.toggle('actice-top')
+})
+
+openModale.addEventListener('click', () =>{
+    modale.classList.add('modale-show')
+    
+})
+closeModale.addEventListener('click', () =>{
+    modale.classList.remove('modale-show')
+
 })
