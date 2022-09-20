11a7507d461ec7e9d34c115fd717df325635f13a
charo7507 <charolite.technologie@gmail.com>
2022-09-20 11:07:49 +0200

    init projet

---
+++ app.js
@@ -0,0 +1,6 @@
+const oppenToggle = document.querySelector('.menu--item')
+const topww = document.querySelector('.top')
+
+oppenToggle.addEventListener('click', () =>{
+    topww.classList.toggle('actice-top')
+})
