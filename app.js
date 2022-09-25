abb8bd4455bb24a5690c46f0510b89c8cbfd3128
charo7507 <charolite.technologie@gmail.com>
2022-09-25 18:13:31 +0200

    visibilitite passzor

--- app.js
+++ app.js
@@ -9,6 +9,32 @@ const modale = document.querySelector('.modale')
 const openModale = document.querySelector('.home__btn')
 const closeModale = document.querySelector('.icon__close')
 
+const eyes = document.querySelector('.input-box i')
+
+
+const pwd = document.getElementById('input-doc')
+
+
+eyes.addEventListener('click', (e)=>{
+
+    if(eyes.classList.contains("bi-eye-fill")){
+        pwd.setAttribute('type', 'text')
+        eyes.classList.replace("bi-eye-fill", "bi-eye-slash-fill")
+
+    }else if(eyes.classList.contains("bi-eye-slash-fill")){
+        pwd.setAttribute('type', 'password')
+        eyes.classList.replace("bi-eye-slash-fill", "bi-eye-fill")
+  
+    }
+})
+
+//<i class="bi bi-eye-slash-fill"></i>
+
+//<i class="bi bi-eye-fill"></i>
+
+
+
+
 oppenToggle.addEventListener('click', () =>{
     topww.classList.toggle('actice-top')
 })
